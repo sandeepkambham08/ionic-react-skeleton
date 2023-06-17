2f7c3c20edb4892c91e70c2d8e76003bb542d66b
sandeep <sandeepkambham08@gmail.com>
2023-06-17 01:09:48 -0400

    ✨ Add tailwind

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,23 +1,30 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar
+} from '@ionic/react';
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader className="bg-green-800">
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle>
+            <div className="bg-red-200 rounded-full p-4">Page title</div>
+          </IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <div className="h-screen text-center bg-red-800 sm:bg-green-400 lg:max-w-screen-xl mx-auto">
+          <div className="border rounded-xl shadow-2xl p-8">
+            <h1 className="text-2xl font-bold">Tab1 with Tailwind</h1>
+            <p className="mt-4"></p>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,jsx,ts,tsx,css}'],
+  darkMode: 'media',
+  theme: {
+    extend: {}
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
